e432c383dcad42d5769f8822abe173ffc26edc46
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-08-07 11:15:45 +0300

    Описаны автотесты JUnit и TestNG

--- IntelliJIDEA/src/test/java/tests/acceptance/TestSampleTestNG.java
+++ IntelliJIDEA/src/test/java/tests/acceptance/TestSampleTestNG.java
@@ -1,4 +1,32 @@
 package tests.acceptance;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import Support.Helper.Selenium;
+import Support.PageObjects.GooglePage;
+import Support.StepObjects.GoogleSteps;
+
 public class TestSampleTestNG {
+    @BeforeTest
+    public static void setup() {
+        Selenium.initWebDriver();
+        Selenium.browserFullScreen();
+    }
+
+    @Test
+    public void testSearch() {
+        Selenium.driver.get("https://www.google.com/");
+        GoogleSteps tester = new GoogleSteps(Selenium.driver);
+        tester.setValueInSearch("GeForce 1650", GooglePage.inputSearchName);
+        int result = tester.getCountResultSearch(GooglePage.searchResultsClass);
+        Assert.assertNotEquals(0, result);
+        System.out.println("Tests finished: SUCCESS");
+    }
+
+    @AfterTest
+    public static void tearDown() {
+        Selenium.quitWebDriver();
+    }
 }
